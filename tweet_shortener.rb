7dca2acbf6162b2d560e9f64d4ef13e46f6b871d
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-07 23:12:32 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,11 +18,7 @@ def word_substituter(string)
   long_tweet = string.split(" ")
   short = []
   long_tweet.each do |word|
-    if dictionary.keys.include?(word)
-      short << dictionary[word]
-    else
-      short << word
-    end
+    dictionary.keys.include?(word) ? short << dictionary[word] : short << word
   end
   return a.join(" ")
 end
