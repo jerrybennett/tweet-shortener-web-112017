de02b35051cd222d2404765ae2f51ff71300439b
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-08 02:38:50 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -32,9 +32,9 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet != nil && tweet.length >= 140
+  if tweet.length >= 140
     word_substituter(tweet)
-  elsif tweet != nil && tweet.length < 130
+  else
     tweet
   end
 end
